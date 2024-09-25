e6aeac918d8755b1335dce3149e0c234198aebe8
Pavel Belokon <pbelokon@myseneca.ca>
2024-09-25 00:15:28 -0400

    small clean up and deletion tool added

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
+//############[THREEJS - RENDERER]############
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+//############[TOOLS]############
 import Entity from "./ECS/Entity";
 import { loadComponent } from "./scripts/scripts";
-import { addCube } from "./scripts/objectsScripts";
+import { addCube, delete_element } from "./scripts/objectsScripts";
 
-//############[Base]############
-
+//############[WINDOW]############
+// Origin
+const origin = {
+  x: 0,
+  y: 0,
+  z: 0,
+};
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// Sizes
-const sizes = {
+// Window Sizes
+const window_sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
@@ -18,17 +26,18 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-// Object
+// BOX 1
 const mesh = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
 );
+
 scene.add(mesh);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
   75,
-  sizes.width / sizes.height,
+  window_sizes.width / window_sizes.height,
   0.1,
   100,
 );
@@ -57,25 +66,33 @@ const mesh2 = new THREE.Mesh(
   new THREE.MeshBasicMaterial({ color: "green" }),
 );
 
+// EXAMPLE - Adding geometry
 mesh2.name = "Box Geometry";
 console.log(mesh2.name);
 example.addComponent(mesh2);
 scene.add(example.components[0]);
 example.components[0].position.x = 2;
-renderer.setSize(sizes.width, sizes.height);
+renderer.setSize(window_sizes.width, window_sizes.height);
 
 loadComponent(example.components[0]);
 
 // Spawn Object
 const spawn = document.querySelector("#spawn");
 spawn.addEventListener("click", () => {
-  addCube(scene);
+  addCube(scene, origin);
+});
+
+// Delete
+const delete_button = document.querySelector("#delete");
+delete_button.addEventListener("click", () => {
+  delete_element(scene, selectedObject);
 });
 
 //############[Selector]############
 
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
+let selectedObject; // current selected object
 
 document.addEventListener("mousedown", (event) => {
   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
@@ -86,7 +103,7 @@ document.addEventListener("mousedown", (event) => {
   const intersections = raycaster.intersectObjects(scene.children, true);
 
   if (intersections.length > 0) {
-    const selectedObject = intersections[0].object;
+    selectedObject = intersections[0].object;
     const color = new THREE.Color(Math.random(), Math.random(), Math.random());
     selectedObject.material.color = color;
     console.log("Selected Object:", selectedObject);
@@ -102,8 +119,6 @@ window.addEventListener("contextmenu", function (event) {
   contextmenu.style.left = event.offsetX + "px";
   contextmenu.classList.add("active");
 });
-
-//############[Delete]############
 
 //############[Animate]############
 
--- src/scripts/objectsScripts.js
+++ src/scripts/objectsScripts.js
@@ -1,12 +1,19 @@
 import * as THREE from "three";
 
-function addCube(scene) {
+function addCube(scene, origin) {
   const newCube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: "purple" }),
   );
 
+  newCube.position.x = origin.x += 1;
   scene.add(newCube);
 }
 
-export { addCube };
+function delete_element(scene, selected_object) {
+  if (selected_object !== null) {
+    scene.remove(selected_object);
+  }
+}
+
+export { addCube, delete_element };
